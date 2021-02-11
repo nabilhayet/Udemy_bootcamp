5f05a04a81d88fc038205e3917c2875bd357f8fc
nabilhayet <nabil4457@yahoo.com>
2021-02-11 16:45:54 -0500

    added js function example

---
+++ fifteenth_lecture/index.js
@@ -0,0 +1,13 @@
+let bestColor = 'purple';
+let quote = 'You had me at "hello"';
+
+const message = "    TASTE THE RAINBOW!  ";
+let whisper = message.trim().toLowerCase();
+
+const word = "skateboard";
+let facialHair = word.slice(5).replace('o', 'e');
+
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+const roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`
