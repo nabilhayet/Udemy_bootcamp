5a465e43a28f95b57c3346b1e6f606e3854dcd9a
nabilhayet <nabil4457@yahoo.com>
2021-02-12 21:54:35 -0500

    added array examples

---
+++ seventeenth_lecture/index.js
@@ -0,0 +1,21 @@
+const lottoNumbers = [3, 5, 7, 9, 1, 0];
+
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix'];
+leaderboard[1] = 'Luna';
+leaderboard[3] = 'Draco';
+
+const planets = ['The Moon', 'Venus', 'Earth', 'Mars', 'Jupiter'];
+
+planets.shift(); // removes first element
+planets.push('Saturn'); // push saturn at the end
+planets.unshift('Mercury'); // Places Mercury at the beginning
+
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+airplaneSeats[3][1] = 'Hugo'
+
