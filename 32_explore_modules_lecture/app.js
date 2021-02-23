b2db5e0795d3f744cb74c417a04c70cfeecc3400
nabilhayet <nabil4457@yahoo.com>
2021-02-22 19:47:29 -0500

    added modules directory examples

---
+++ 32_explore_modules_lecture/app.js
@@ -0,0 +1,7 @@
+const index = require('./index')
+// console.log(index.PI)
+
+const cats = require('./shelter')
+
+// console.log(index.square(9))
+console.log(cats)
---
+++ 32_explore_modules_lecture/index.js
@@ -0,0 +1,12 @@
+const add = (x, y) => x + y
+const PI = 3.14159;
+const square = x => x * x;
+
+const math = {
+    add: add,
+    PI: PI,
+    square: square
+}
+
+module.exports = math
+
