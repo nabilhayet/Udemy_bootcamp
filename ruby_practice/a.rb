fb688af840543517f687a17135a558e9ac639dee
nabilhayet <nabil4457@yahoo.com>
2021-06-23 12:32:03 -0400

    added methods examples

--- ruby_practice/a.rb
+++ ruby_practice/a.rb
@@ -38,3 +38,39 @@ def parrot(name="Squawk!")
 end 
 
 parrot("Wack Wack")
+
+all_odd = true
+[1,2,3].each do |number|
+  if number.even? # Will evaluate to false for 1, true for 2, false for 3
+    all_odd = false
+  end
+end
+all_odd 
+
+all_odd = [1,3].all? do |number|
+    number.odd? # Will evaluate to true for 1, true for 3
+end #=> true
+all_odd
+
+[1,3].none?{|i| i.even?} #=> true
+
+[1,2,100].any?{|i| i > 99} #=> true
+
+the_numbers = [4,8,15,16,23,42]
+the_numbers.include?(42)   #=> true
+
+matches = []
+[1,2,3,4,5].each do |i|
+  matches << i if i.even? # add i to the matches array if it is even
+end #=> [1,2,3,4,5]
+matches #=> [2,4]
+
+[1,2,3,4,5].select{|i| i.odd?} #=> [1,3,5]
+ 
+[1,2,3].select{|i| i.is_a?(String)} #=> []
+
+[1,2,3].detect{|i| i.odd?} #=> 1
+
+[1,2,3].find{|i| i.odd?} #=> 1
+
+[1,2].reject{|i| i.even?} #=> [1]
