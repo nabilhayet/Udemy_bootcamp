cebca783bec7aa0ffed28ecf8d105a2d3b0bd240
nabilhayet <nabil4457@yahoo.com>
2021-11-10 23:17:07 -0500

    added code for static html

--- 34_creating_dynamic_html_with_template/index.js
+++ 34_creating_dynamic_html_with_template/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 const path = require('path')
+const redditData = require('./data.json')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
@@ -9,9 +13,20 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/cats', (req, res) => {
+    const cats = ["Blue", "Rocket", "Monty", "Stephine", "Nabil"]
+    res.render('cats', { cats })
+})
+
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params
-    res.render('subreddit', { subreddit })
+    const data = redditData[subreddit]
+    if (data) {
+        res.render('subreddit', { ...data })
+    } else {
+        res.render('notfound', { subreddit })
+    }
+
 })
 
 app.get('/rand', (req, res) => {
