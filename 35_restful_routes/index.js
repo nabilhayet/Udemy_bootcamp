6f4a99836523063e0b180f9b520fb73c463b0542
nabilhayet <nabil4457@yahoo.com>
2021-11-14 14:56:35 -0500

    added full crud routes

--- 35_restful_routes/index.js
+++ 35_restful_routes/index.js
@@ -1,26 +1,33 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const methodOverride = require('method-override')
+const { v4: uuid } = require('uuid')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 const comments = [
     {
+        id: uuid(),
         username: 'Todd',
         comment: 'lol haha'
     },
     {
+        id: uuid(),
         username: 'Bob',
         comment: 'Ola komostas'
     },
     {
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like gardenning'
     },
     {
+        id: uuid(),
         username: 'Park',
         comment: 'Woof woof woff'
     }
@@ -36,10 +43,36 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body
-    comments.push({ username, comment })
+    comments.push({ username, comment, id: uuid() })
     res.redirect('/comments')
 })
 
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params
+    const comment = comments.find(c => c.id === (id))
+    res.render('comments/show', { comment })
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/edit', { comment })
+})
+
+app.patch('/comments/:id', (req, res) => {
+    debugger
+    const { id } = req.params
+    const newComment = req.body.comment
+    const foundComment = comments.find(c => c.id === id)
+    foundComment.comment = newComment
+    res.redirect('/comments')
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params
+    comments = comments.filter(c => c.id !== id)
+    res.redirect('/comments')
+})
 
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
