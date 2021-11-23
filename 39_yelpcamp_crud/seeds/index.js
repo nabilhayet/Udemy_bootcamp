a62bc64c9b7103aa99b53236c16add0ffa361286
nabilhayet <nabil4457@yahoo.com>
2021-11-22 21:22:53 -0500

    added bootstrap styling

--- 39_yelpcamp_crud/seeds/index.js
+++ 39_yelpcamp_crud/seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Maxime impedit doloremque cupiditate eos voluptate architecto animi ducimus, beatae rem ratione non aut? Tempora perspiciatis provident, blanditiis ullam veritatis adipisci impedit.",
+            price: price
         })
         await camp.save();
     }
