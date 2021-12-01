60f9f9dc7370656d4c2e1f046fa17a5110b9716f
nabilhayet <nabil4457@yahoo.com>
2021-11-30 22:24:20 -0500

    added farm model and nested routes for products

--- 38_mongoose_with_express/index.js
+++ 38_mongoose_with_express/index.js
@@ -6,8 +6,9 @@ const methodOverride = require('method-override')
 
 
 const Product = require('./models/product')
+const Farm = require('./models/farm')
 
-mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/farmStand2', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connection Open!')
     })
@@ -23,6 +24,50 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 const categories = ['fruit', 'vegetable', 'dairy']
+
+//// FARM ROUTES
+
+app.get('/farms', async (req, res) => {
+    const farms = await Farm.find({})
+    res.render('farms/index', { farms })
+})
+
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new')
+})
+
+app.post('/farms', async (req, res) => {
+    const farm = new Farm(req.body)
+    await farm.save()
+    res.redirect('/farms')
+})
+
+app.get('/farms/:id/products/new', async (req, res) => {
+    const { id } = req.params
+    const farm = await Farm.findById(id)
+    res.render('products/new', { categories, farm })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params
+    const farm = await Farm.findById(id)
+    const { name, price, category } = req.body
+    const product = new Product({ name, price, category })
+    farm.products.push(product)
+    product.farm = farm
+    await farm.save()
+    await product.save()
+    res.redirect(`/farms/${id}`)
+})
+
+app.get('/farms/:id', async (req, res) => {
+    const { id } = req.params
+    const farm = await Farm.findById(id).populate('products')
+    res.render('farms/show', { farm })
+})
+
+
+//// PRODUCT ROUTES
 
 app.get('/products', async (req, res) => {
     const { category } = req.query
@@ -67,7 +112,7 @@ app.delete('/products/:id', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params
-    const product = await Product.findById(id)
+    const product = await Product.findById(id).populate('farm', 'name')
     res.render('products/show', { product })
 })
 
